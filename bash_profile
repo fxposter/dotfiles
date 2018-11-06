7591425e01b1a9c90ee45bcf2384365de92d825c
Pavel Forkert <fxposter@gmail.com>
2018-11-06 17:50:48 +0200

    Updates

--- bash_profile
+++ bash_profile
@@ -1,20 +1,20 @@
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export GOPATH=$HOME/go
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/go/bin:/usr/X11/bin
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/go/bin:/usr/X11/bin:/usr/local/share/dotnet
 export EDITOR="lime -w"
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/Home
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export HISTCONTROL=ignoredups:erasedups
 export HISTSIZE=5000
 shopt -s histappend
 
+source "$HOME/.bash/ruby"
 source "$HOME/.bash/completion"
 source "$HOME/.bash/git"
 source "$HOME/.bash/aliases"
 source "$HOME/.bash/prompt"
 source "$HOME/.bash/colors"
-source "$HOME/.bash/ruby"
 source "$HOME/.bash/diff"
 source "$HOME/.bash/docker"
 [[ -f "$HOME/.bashrc_local" ]] && source "$HOME/.bashrc_local"
@@ -22,4 +22,6 @@ source "$HOME/.bash/docker"
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
 
-source "$HOME/.iterm2_shell_integration.bash"
+# source "$HOME/.iterm2_shell_integration.bash"
+
+[[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
--- macosx/phoenix.js
+++ macosx/phoenix.js
@@ -19,8 +19,8 @@ function moveToScreen(window, screen) {
   }
 
   var frame = window.frame();
-  var oldScreenRect = window.screen().frameWithoutDockOrMenu();
-  var newScreenRect = screen.frameWithoutDockOrMenu();
+  var oldScreenRect = window.screen().frame();
+  var newScreenRect = screen.flippedVisibleFrame();
 
   var xRatio = newScreenRect.width / oldScreenRect.width;
   var yRatio = newScreenRect.height / oldScreenRect.height;
@@ -34,20 +34,70 @@ function moveToScreen(window, screen) {
 }
 
 function toFullScreen(window) {
-  var screen = window.screen().frameWithoutDockOrMenu();
-  window.setFrame({
-    x: screen.x,
-    y: screen.y,
-    width: screen.width,
-    height: screen.height
-  });
+  if (window) {
+    var screen = window.screen().flippedVisibleFrame();
+    window.setFrame({
+      x: screen.x,
+      y: screen.y,
+      width: screen.width,
+      height: screen.height
+    });
+  }
+}
+
+function toLeftHalfOfScreen(window) {
+  if (window) {
+    var screen = window.screen().frame();
+    window.setFrame({
+      x: screen.x,
+      y: screen.y,
+      width: screen.width / 2,
+      height: screen.height
+    });
+  }
+}
+
+function toRightHalfOfScreen(window) {
+  if (window) {
+    var screen = window.screen().frame();
+    window.setFrame({
+      x: screen.x + screen.width / 2,
+      y: screen.y,
+      width: screen.width / 2,
+      height: screen.height
+    });
+  }
+}
+
+function toTopHalfOfScreen(window) {
+  if (window) {
+    var screen = window.screen().flippedVisibleFrame();
+    window.setFrame({
+      x: screen.x,
+      y: screen.y,
+      width: screen.width,
+      height: screen.height / 2
+    });
+  }
+}
+
+function toBottomHalfOfScreen(window) {
+  if (window) {
+    var screen = window.screen().flippedVisibleFrame();
+    window.setFrame({
+      x: screen.x,
+      y: screen.y + screen.height / 2,
+      width: screen.width,
+      height: screen.height / 2
+    });
+  }
 }
 
 function appByTitle(title, callback) {
-  var apps = App.runningApps();
+  var apps = App.all();
   for (i = 0; i < apps.length; i++) {
     var app = apps[i];
-    if (app.title() == title) {
+    if (app.name() == title) {
       app.show();
       callback(app);
     }
@@ -55,7 +105,7 @@ function appByTitle(title, callback) {
 }
 
 function firstWindow(app) {
-  return app.visibleWindows()[0];
+  return app.windows({ "visible": true })[0];
 }
 
 function eq(expected) {
@@ -64,9 +114,9 @@ function eq(expected) {
   }
 }
 
-function notEq(expected) {
+function not(matcher) {
   return function(actual) {
-    return actual != expected;
+    return !matcher(actual);
   }
 }
 
@@ -78,9 +128,9 @@ function method(name) {
 
 function windowWith(valueExtractor, valueMatcher) {
   return function(app) {
-    var win;
-    for (var i in app.allWindows()) {
-      win = app.allWindows()[i];
+    var windows = app.windows(), win;
+    for (var i in windows) {
+      win = windows[i];
       if (valueMatcher(valueExtractor(win))) {
         return win;
       }
@@ -103,46 +153,76 @@ function resize(windowMatcher, x, y, width, height) {
   }
 }
 
-api.bind('m', ['cmd', 'alt', 'ctrl'], function() {
-  toFullScreen(Window.focusedWindow());
+var m = new Key('m', ['cmd', 'ctrl', 'alt'], function () {
+  toFullScreen(Window.focused());
 });
 
-api.bind('right', ['ctrl', 'alt'], function() {
-  var win = Window.focusedWindow();
-  moveToScreen(win, win.screen().previousScreen());
+// api.bind('right', ['ctrl', 'alt'], function() {
+//   var win = Window.focused();
+//   moveToScreen(win, win.screen().previousScreen());
+// });
+
+// api.bind('left', ['ctrl', 'alt'], function() {
+//   var win = Window.focused();
+//   moveToScreen(win, win.screen().nextScreen());
+// });
+
+// api.bind('4', ['cmd', 'ctrl', 'alt'], function() {
+//   var apps = App.runningApps(), app, frame, win;
+//   var result = [];
+//   for (var i in apps) {
+//     app = apps[i];
+//     win = app.visibleWindows()[0];
+//     if (win) {
+//       frame = win.frame();
+//       result.push("appByTitle('" + app.title() + "', resize(firstWindow, " + frame.x + ", " + frame.y + ", " + frame.width + ", " + frame.height + "))");
+//     }
+//   }
+//   api.alert(result.join("\n"), 10);
+// });
+
+var lh = new Key('left', ['cmd', 'ctrl', 'alt'], function () {
+  toLeftHalfOfScreen(Window.focused());
 });
 
-api.bind('left', ['ctrl', 'alt'], function() {
-  var win = Window.focusedWindow();
-  moveToScreen(win, win.screen().nextScreen());
+var rh = new Key('right', ['cmd', 'ctrl', 'alt'], function () {
+  toRightHalfOfScreen(Window.focused());
 });
 
-api.bind('4', ['cmd', 'ctrl', 'alt'], function() {
-  var apps = App.runningApps(), app, frame, win;
-  var result = [];
-  for (var i in apps) {
-    app = apps[i];
-    win = app.visibleWindows()[0];
-    if (win) {
-      frame = win.frame();
-      result.push("appByTitle('" + app.title() + "', resize(firstWindow, " + frame.x + ", " + frame.y + ", " + frame.width + ", " + frame.height + "))");
-    }
-  }
-  api.alert(result.join("\n"), 10);
+var uh = new Key('up', ['cmd', 'ctrl', 'alt'], function () {
+  toTopHalfOfScreen(Window.focused());
 });
 
-api.bind('3', ['cmd', 'ctrl', 'alt'], function() {
-  appByTitle('iTerm2', resize(firstWindow, -1440, 202, 1440, 878));
+var bh = new Key('down', ['cmd', 'ctrl', 'alt'], function () {
+  toBottomHalfOfScreen(Window.focused());
+});
+
+function screenByIndex(index) {
+  return Screen.all().sort(function(screen1, screen2) {
+    return screen1.frame().x - screen2.frame().x;
+  })[index];
+}
+
+var organize = new Key('3', ['cmd', 'ctrl', 'alt'], function () {
+  appByTitle('iTerm2', function(app) {
+    app.mainWindow().setFrame(screenByIndex(0).flippedVisibleFrame());
+  });
+
+  // appByTitle('iTerm2', resize(firstWindow, -1440, 202, 1440, 878));
 
   appByTitle('Airmail', resize(firstWindow, 300, 216, 1362, 622));
-  appByTitle('ReadKit', resize(firstWindow, 606, 72, 1036, 653));
+  appByTitle('ReadKit', resize(firstWindow, 606, 72, 1148, 653));
 
   appByTitle('Nightly', compose(toFullScreen, firstWindow));
 
-  appByTitle('Sublime Text', resize(firstWindow, 0, 22, 1920, 988));
+  appByTitle('Sublime Text', resize(firstWindow, 0, 23, 1920, 1125));
 
   appByTitle('Twitter', resize(firstWindow, 1974, 42, 500, 750));
   appByTitle('Adium', resize(windowWith(method('title'), eq('Contacts')), 2572, 42, 250, 750));
-  appByTitle('Adium', resize(windowWith(method('title'), notEq('Contacts')), 2372, 450, 750, 500));
+  appByTitle('Adium', resize(windowWith(method('title'), not(eq('Contacts'))), 2372, 450, 750, 500));
   appByTitle('Skype', resize(firstWindow, 2902, 42, 900, 750));
 });
+
+// api.bind('3', ['cmd', 'ctrl', 'alt'], function() {
+
+// });
